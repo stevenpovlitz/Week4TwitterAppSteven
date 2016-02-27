8f20db14e3ac14d5b4b8e682b7b307a52ed89c87
Steven Povlitz <steven.povlitz@gmail.com>
2016-02-27 17:22:15 -0500

    some of the layout done for composing a tweet

---
+++ TwitterDemoSteven/ComposeViewController.swift
@@ -0,0 +1,13 @@
+//
+//  ComposeViewController.swift
+//  TwitterDemoSteven
+//
+//  Created by Steven Povlitz on 2/27/16.
+//  Copyright © 2016 Steven Povlitz. All rights reserved.
+//
+
+import Cocoa
+
+class ComposeViewController: UITableViewCell {
+
+}
