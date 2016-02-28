03777e241172a0119cf735f3ac0e42b94cda2ef8
Steven Povlitz <steven.povlitz@gmail.com>
2016-02-27 22:54:28 -0500

    updated warning message as comment for compose view

--- TwitterDemoSteven/ComposeViewController.swift
+++ TwitterDemoSteven/ComposeViewController.swift
@@ -10,6 +10,14 @@ import UIKit
 
 class ComposeViewController: UIViewController {
     
+    /* NOTE: some weirdness cause I created the class incorrectly: 
+    initially had "Compo" as in 'ComposeViewController: Compo' but I changed 
+    that to UIViewController like it should be. Now I get the error:
+    'Unknown class Compo in Interface Builder file.' 
+    Class still works but should probably change that
+    */
+
+
     var tweet: Tweet?
     
     @IBOutlet weak var centerTextField: UITextView!
